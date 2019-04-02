2cc2cfa00f26cc3e273714f5a5364ba177d0dce0
Karen9711 <bearbear199711@qq.com>
2019-04-02 20:04:12 +0800

    js file

--- js/booklistJS.js
+++ js/booklistJS.js
@@ -1,14 +1,1 @@
-var booklist = new Vue({
-  el:'#booklist',
-  data:{
-    book:'book',
-    booklist:[1],
-  },
-  methods:{
-    onClick:function(){
-      bookname = booklist.$data.book;
-      console.log(bookname);
-      this.$data.booklist.push(bookname);
-    },
-  }
-})
+
